681439c7f5a3c8706837dc60efbfa76177830e4a
caioagralemos <caioagralemos@live.com>
2023-12-14 18:36:09 -0300

    alpha 1.2
    functions skeleton added

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 
 public class Cliente extends Usuario {
+    double saldo;
     ArrayList<Aluguel> alu_atuais;
     ArrayList<Aluguel> alu_concluidos;
 
     public Cliente(String nome, String username, String senha) {
         super(nome, username, senha);
+        this.saldo = 0;
         this.alu_atuais = new ArrayList<>();
         this.alu_concluidos = new ArrayList<>();
     }
--- src/Pagina.java
+++ src/Pagina.java
@@ -1,6 +1,5 @@
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -23,7 +22,7 @@ public class Pagina {
 
     public Pagina() {
         try {
-            Path admin_path = Paths.get("./data/admin.json");
+            Path admin_path = Paths.get("./data/admins.json");
             byte[] admin_jsonData = Files.readAllBytes(admin_path);
             String admin_json = new String(admin_jsonData);
             admins = gson.fromJson(admin_json, new TypeToken<ArrayList<Admin>>(){}.getType());
@@ -41,6 +40,28 @@ public class Pagina {
             byte[] clientes_jsonData = Files.readAllBytes(clientes_path);
             String clientes_json = new String(clientes_jsonData);
             clientes = gson.fromJson(clientes_json, new TypeToken<ArrayList<Cliente>>(){}.getType());
+        } catch (Exception ignored) {}
+
+
+        try {
+            Path livros_path = Paths.get("./data/livros.json");
+            byte[] livros_jsonData = Files.readAllBytes(livros_path);
+            String livros_json = new String(livros_jsonData);
+            livros = gson.fromJson(livros_json, new TypeToken<ArrayList<Livro>>(){}.getType());
+        } catch (Exception ignored) {}
+
+        try {
+            Path alugueis_path = Paths.get("./data/alugueis.json");
+            byte[] alugueis_jsonData = Files.readAllBytes(alugueis_path);
+            String alugueis_json = new String(alugueis_jsonData);
+            alugueis = gson.fromJson(alugueis_json, new TypeToken<ArrayList<Aluguel>>(){}.getType());
+        } catch (Exception ignored) {}
+
+        try {
+            Path concluidos_path = Paths.get("./data/concluidos.json");
+            byte[] concluidos_jsonData = Files.readAllBytes(concluidos_path);
+            String concluidos_json = new String(concluidos_jsonData);
+            concluidos = gson.fromJson(concluidos_json, new TypeToken<ArrayList<Aluguel>>(){}.getType());
         } catch (Exception ignored) {}
 
         System.out.println("\nBem vindo(a) ao Página.");
@@ -69,7 +90,7 @@ public class Pagina {
                                 usuario = c;
                                 off = true;
                                 acc_found = true;
-                                break;
+                                clientInterface();
                             } else {
                                 output("Senha incorreta. Tente novamente\n");
                             }
@@ -89,7 +110,7 @@ public class Pagina {
                         clientes.add(novo_user);
                         usuario = novo_user;
                         off = true;
-                        break;
+                        clientInterface();
                     } catch (Exception e) {
                         output("Algo deu errado. Tente novamente");
                     }
@@ -108,12 +129,12 @@ public class Pagina {
                         clientes.add(novo_user);
                         usuario = novo_user;
                         off = true;
-                        break;
+                        clientInterface();
                     } catch (Exception e) {
                         output("Algo deu errado. Tente novamente");
                     }
                 }
-                if (off) { break; }
+                if (off) { clientInterface(); }
             }
         } else if (escolha.equals("2")) {
             while (true) {
@@ -129,7 +150,7 @@ public class Pagina {
                                 usuario = c;
                                 off = true;
                                 acc_found = true;
-                                break;
+                                libInterface();
                             } else {
                                 output("Senha incorreta. Tente novamente\n");
                             }
@@ -149,7 +170,7 @@ public class Pagina {
                         bibliotecas.add(novo_user);
                         usuario = novo_user;
                         off = true;
-                        break;
+                        libInterface();
                     } catch (Exception e) {
                         output("Algo deu errado. Tente novamente");
                     }
@@ -173,7 +194,7 @@ public class Pagina {
                         output("Algo deu errado. Tente novamente");
                     }
                 }
-                if (off) { break; }
+                if (off) { libInterface(); }
             }
         } else {
             while (true) {
@@ -185,86 +206,208 @@ public class Pagina {
                 if (!admins.isEmpty()) {
                     for(Admin c: admins) {
                         if (c.getUsername().equals(username)) {
+                            acc_found = true;
                             if (c.login()) {
                                 usuario = c;
                                 off = true;
-                                acc_found = true;
-                                break;
+                                adminInterface();
                             } else {
                                 output("Senha incorreta. Tente novamente\n");
                             }
                         }
                     }
+                    if (!acc_found) {
+                        output("Não foi encontrado admin com esse username.\n");
+                    }
                 } else {
-                    output("Não foi encontrada uma conta com esse nome. Criando nova conta");
-
-                    System.out.print("Digite seu nome: ");
-                    String nome = scanner.nextLine();
-
-                    System.out.print("Digite sua nova senha: ");
-                    String password = scanner.nextLine();
-
-                    try {
-                        Admin novo_user = new Admin(nome, username, password);
-                        admins.add(novo_user);
-                        usuario = novo_user;
-                        off = true;
-                        break;
-                    } catch (Exception e) {
-                        output("Algo deu errado. Tente novamente");
-                    }
+                    output("Não há admins registrados.\n");
                 }
-                if (!acc_found) {
-                    output("Não foi encontrada uma conta com esse nome. Criando nova conta");
-
-                    System.out.print("Digite seu nome: ");
-                    String nome = scanner.nextLine();
-
-                    System.out.print("Digite sua nova senha: ");
-                    String password = scanner.nextLine();
-
-                    try {
-                        Admin novo_user = new Admin(nome, username, password);
-                        admins.add(novo_user);
-                        usuario = novo_user;
-                        off = true;
-                        break;
-                    } catch (Exception e) {
-                        output("Algo deu errado. Tente novamente");
-                    }
-                }
-                if (off) { break; }
+                if (off) { adminInterface(); }
             }
         }
-
-        try {
-            Path livros_path = Paths.get("./data/livros.json");
-            byte[] livros_jsonData = Files.readAllBytes(livros_path);
-            String livros_json = new String(livros_jsonData);
-            livros = gson.fromJson(livros_json, new TypeToken<ArrayList<Livro>>(){}.getType());
-        } catch (Exception ignored) {}
-
-        try {
-            Path alugueis_path = Paths.get("./data/alugueis.json");
-            byte[] alugueis_jsonData = Files.readAllBytes(alugueis_path);
-            String alugueis_json = new String(alugueis_jsonData);
-            alugueis = gson.fromJson(alugueis_json, new TypeToken<ArrayList<Aluguel>>(){}.getType());
-        } catch (Exception ignored) {}
-
-        try {
-            Path concluidos_path = Paths.get("./data/concluidos.json");
-            byte[] concluidos_jsonData = Files.readAllBytes(concluidos_path);
-            String concluidos_json = new String(concluidos_jsonData);
-            concluidos = gson.fromJson(concluidos_json, new TypeToken<ArrayList<Aluguel>>(){}.getType());
-        } catch (Exception ignored) {}
-
-        userInterface();
 
     }
 
     private void output(String texto) { System.out.println("Página diz: " + texto); }
-    private void userInterface() {
-        System.out.println(usuario);
+
+    private Livro pesquisarLivro() {
+        if (livros.isEmpty()) {
+            output("Não há livros cadastrados.");
+            return null;
+        } else {
+            return null;
+        }
+    }
+
+    private void alugarLivroC() {
+        if(usuario.getTipo().equals("class Cliente")) {
+            output("é cliente");
+        }
+    }
+
+    private void gerenciarAlugueisC() {
+        if(usuario.getTipo().equals("class Cliente")) {
+            output("é cliente");
+        }
+    }
+
+    private void gerenciarContaC() {
+        if(usuario.getTipo().equals("class Cliente")) {
+            output("é cliente");
+        }
+    }
+
+    private void clientInterface() {
+        while (true) {
+            System.out.println();
+            output("Olá, " + this.usuario.getNome() + "! Você pode me controlar usando os seguintes comandos: ");
+
+            System.out.println("1 - Alugar Livro");
+            System.out.println("2 - Pesquisar Livro");
+            System.out.println("3 - Meus Aluguéis");
+            System.out.println("4 - Gerenciar Conta");
+            System.out.println("S - Fechar e Salvar Alterações\n");
+
+            System.out.print("Digite aqui: ");
+            String escolha = scanner.nextLine();
+
+            if (escolha.equals("1")) {
+                alugarLivroC();
+            } else if (escolha.equals("2")) {
+                pesquisarLivro();
+            } else if (escolha.equals("3")) {
+                gerenciarAlugueisC();
+            } else if (escolha.equals("4")) {
+                gerenciarContaC();
+            } else {
+                System.out.print("Digite S para confirmar sua saída do programa: ");
+                String escolha2 = scanner.nextLine().strip().toUpperCase();
+                if (escolha2.equals("S")) {
+                    sair();
+                }
+            }
+        }
+    }
+
+    private void gerenciarUsuariosA() {
+        if(usuario.getTipo().equals("class Admin")) {
+            output("é admin");
+        }
+    }
+
+    private void verAlugueisA() {
+        if(usuario.getTipo().equals("class Admin")) {
+            output("é admin");
+        }
+    }
+
+    private void adicionarAdminA() {
+        if(usuario.getTipo().equals("class Admin")) {
+            output("é admin");
+        }
+    }
+
+    private void adicionarBibliotecaA() {
+        if(usuario.getTipo().equals("class Admin")) {
+            output("é admin");
+        }
+    }
+
+    private void adminInterface() {
+        while (true) {
+            System.out.println();
+            output("Olá, " + this.usuario.getNome() + "! Você pode me controlar usando os seguintes comandos: ");
+
+            System.out.println("1 - Gerenciar Usuários");
+            System.out.println("2 - Gerenciar Alugueis");
+            System.out.println("3 - Adicionar Admin");
+            System.out.println("4 - Adicionar Biblioteca");
+            System.out.println("5 - Pesquisar Livro");
+            System.out.println("S - Fechar e Salvar Alterações\n");
+
+            System.out.print("Digite aqui: ");
+            String escolha = scanner.nextLine();
+
+            if (escolha.equals("1")) {
+                gerenciarUsuariosA();
+            } else if (escolha.equals("2")) {
+                verAlugueisA();
+            } else if (escolha.equals("3")) {
+                adicionarAdminA();
+            } else if (escolha.equals("4")) {
+                adicionarBibliotecaA();
+            } else if (escolha.equals("5")) {
+                pesquisarLivro();
+            } else {
+                System.out.print("Digite S para confirmar sua saída do programa: ");
+                String escolha2 = scanner.nextLine().strip().toUpperCase();
+                if (escolha2.equals("S")) {
+                    sair();
+                }
+            }
+        }
+    }
+
+    private void gerenciarAlugueisB() {
+        if(usuario.getTipo().equals("class Biblioteca")) {
+            output("é biblioteca");
+        }
+    }
+
+    private void adicionarLivroB() {
+        if(usuario.getTipo().equals("class Biblioteca")) {
+            output("é biblioteca");
+        }
+    }
+
+    private void editarLivroB() {
+        if(usuario.getTipo().equals("class Biblioteca")) {
+            output("é biblioteca");
+        }
+    }
+
+    private void gerenciarFinancasB() {
+        if(usuario.getTipo().equals("class Biblioteca")) {
+            output("é biblioteca");
+        }
+    }
+
+    private void libInterface() {
+        while (true) {
+            System.out.println();
+            output("Olá, " + this.usuario.getNome() + "! Você pode me controlar usando os seguintes comandos: ");
+
+            System.out.println("1 - Gerenciar Alugueis");
+            System.out.println("2 - Adicionar Livro");
+            System.out.println("3 - Editar Livro");
+            System.out.println("4 - Gerenciar Finanças");
+            System.out.println("5 - Pesquisar Livro");
+            System.out.println("S - Fechar e Salvar Alterações\n");
+
+            System.out.print("Digite aqui: ");
+            String escolha = scanner.nextLine();
+            
+            if (escolha.equals("1")) {
+                gerenciarAlugueisB();
+            } else if (escolha.equals("2")) {
+                adicionarLivroB();
+            } else if (escolha.equals("3")) {
+                editarLivroB();
+            } else if (escolha.equals("4")) {
+                gerenciarFinancasB();
+            } else if (escolha.equals("5")) {
+                pesquisarLivro();
+            } else {
+                System.out.print("Digite S para confirmar sua saída do programa: ");
+                String escolha2 = scanner.nextLine().strip().toUpperCase();
+                if (escolha2.equals("S")) {
+                    sair();
+                }
+            }
+        }
+    }
+
+    private void sair() {
         output("Tentando salvar os seus dados...");
 
         File dataFolder = new File("./data");
@@ -330,5 +473,6 @@ public class Pagina {
 
 
         output("Obrigado por utilizar o Página.");
+        System.exit(0);
     }
 }
